Fixed loss=nan. TODO: add 16bit precision & optimization scheduler

models.py
@@ -3,6 +3,7 @@ from torch import nn
 from collections import OrderedDict
 from config_parser import config_args
 from torchvision.models.resnet import resnet50
+import copy
 
 
 class ResNet_Encoder(nn.Module):
@@ -45,9 +46,9 @@ class MoCoV2(nn.Module):
 
         original_model = resnet50()
         self.f_q = ResNet_Encoder(feat_dim=self.feat_dim, original_model=original_model)
-        self.f_k = ResNet_Encoder(feat_dim=self.feat_dim, original_model=original_model)
+        self.f_k = copy.deepcopy(self.f_q)
 
-        self.queue = torch.randn((self.queue_len, self.feat_dim))
+        self.queue = torch.randn((self.feat_dim, self.queue_len))
 
 
 if __name__ == '__main__':

trainer.py
@@ -3,19 +3,20 @@ from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 import os
 import time
-
-def InfoNCELoss(q, k, queue, moco_model_args):
+torch.autograd.set_detect_anomaly(True)
+def InfoNCELoss(q, k, queue, criterion, moco_model_args, device):
     T = moco_model_args['temperature']
     N, C = q.shape
     K = queue.shape[0]
 
     # logits
-    l_positive = torch.bmm(q.view(N, 1, C), k.view(N, C, 1)).cpu()  # Nx1
-    l_negative = torch.mm(q.view(N, C).cpu(), queue.view(C, K))  # NxK
+    # l_positive = torch.bmm(q.view(N, 1, C), k.view(N, C, 1))  # Nx1
+    # l_negative = torch.mm(q.view(N, C), queue.view(C, K))  # NxK
+    l_positive = torch.einsum('nc,nc->n', [q, k]).unsqueeze(-1)
+    l_negative = torch.einsum('nc,ck->nk', [q, queue.clone().detach()])
 
-    logits = torch.cat([l_positive.squeeze(-1), l_negative], dim=1).cpu()  # Nx(1+K)
-    labels = torch.zeros((N, ), dtype=torch.long)
-    criterion = torch.nn.CrossEntropyLoss()
+    logits = torch.cat([l_positive, l_negative], dim=1)  # Nx(1+K)
+    labels = torch.zeros((N, ), dtype=torch.long).to(device)
     loss = criterion(logits / T, labels)
 
     return loss
@@ -29,8 +30,8 @@ def requeue(k, queue):
     :param queue:
     :return:
     """
-    queue = torch.cat((queue, k), 0)
-    return queue[k.shape[0]:, :]
+    queue = torch.cat((torch.t(queue), k), 0)
+    return torch.t(queue[k.shape[0]:, :])
 
 
 class Trainer:
@@ -48,8 +49,10 @@ class Trainer:
         self.device = device
         self.aug_transform = aug_transform
         self.moco_model_args = moco_model_args
-        self.queue_train = moco_model.queue
-        self.queue_val = moco_model.queue
+        self.queue_train = moco_model.queue.to(self.device)
+        self.queue_train = torch.nn.functional.normalize(self.queue_train, dim=0)
+        self.queue_val = moco_model.queue.to(self.device)
+        self.queue_val = torch.nn.functional.normalize(self.queue_val, dim=0)
         self.moco_logs_path = moco_model_args['log_path']
         self.writer = SummaryWriter(self.moco_logs_path)
 
@@ -84,39 +87,45 @@ class Trainer:
             self.optimizer.load_state_dict(checkpoints['optimizer_state_dict'])
             best_val_loss = checkpoints['best_val_loss']
             last_epoch = checkpoints['last_epoch']
-
+        decayRate = 0.9
+        scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer,
+                                                    step_size=5,
+                                                    gamma=decayRate,
+                                                    verbose=True)
+        criterion = torch.nn.CrossEntropyLoss()
         for epoch in range(last_epoch, num_epochs):
             startTime = time.time()
             train_epoch_loss = 0
+
             for batch_data in dl_train:
                 # image, label = batch_data['image'].to(self.device), batch_data['label'].to(self.device)
-
                 image = batch_data['image'].to(self.device)
                 image_q = self.aug_transform(image)
                 image_k = self.aug_transform(image)
                 # encoders
                 with torch.no_grad():
-                    k = self.f_k(image_k).to(self.device)
+                    k = self.f_k(image_k)
                     # moment
                     self.momentum_update()
 
-                q = self.f_q(image_q).to(self.device)
+                q = self.f_q(image_q)
 
                 k.detach()  # update only with momentum
+                with torch.autograd.detect_anomaly():
+                    self.optimizer.zero_grad()
 
-                self.optimizer.zero_grad()
+                    # Zero gradients, perform a backward pass,
+                    # and update the weights.
+                    loss = InfoNCELoss(q, k, self.queue_train, criterion, self.moco_model_args, device=self.device)
 
-                # Zero gradients, perform a backward pass,
-                # and update the weights.
-                loss = InfoNCELoss(q, k, self.queue_train.cpu(), self.moco_model_args)
-                # loss.backward()
+                    loss.backward()
 
-                loss.backward()
+                    self.optimizer.step()
 
-                self.optimizer.step()
-                train_epoch_loss += loss.item()
-                # queue update
-                self.queue_train = requeue(k, self.queue_train.to(self.device))
+                    train_epoch_loss += loss.item()
+                    # queue update
+                    self.queue_train = requeue(k, self.queue_train)
+            scheduler.step()
             endTime = time.time()
 
             avg_train_loss = train_epoch_loss / len(dl_train)
@@ -155,6 +164,7 @@ class Trainer:
         Returns: f1 score
         """
         val_loss = 0
+        criterion = torch.nn.CrossEntropyLoss()
         with torch.no_grad():
             for val_data in dl_val:
                 # image_val, label_val = val_data['image'].to(self.device), val_data['label'].to(self.device)
@@ -165,7 +175,7 @@ class Trainer:
                 k = self.f_k(image_k)
                 q = self.f_q(image_q)
                 #
-                loss = InfoNCELoss(q, k, self.queue_val.cpu(), self.moco_model_args)
+                loss = InfoNCELoss(q, k, self.queue_val, criterion, self.moco_model_args, device=self.device)
                 val_loss += loss.item()
 
                 self.queue_val = requeue(k, self.queue_val.to(self.device))
@@ -176,3 +186,4 @@ class Trainer:
         m = self.moco_model_args['momentum']
         for theta_q, theta_k in zip(self.f_q.parameters(), self.f_k.parameters()):
             theta_k.data = theta_k.data * m + theta_q.data * (1. - m)
+            theta_k.requires_grad = False